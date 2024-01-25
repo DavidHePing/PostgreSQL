24cec0f66b56c4468f3063c285e41a407e026c6a
Dav He <s991179@gmail.com>
2024-01-25 09:10:21 +0800

    feat: table add update_at

--- Create.sql
+++ Create.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS public."cars"
     "id" serial NOT NULL,
     "name" text NOT NULL,
     "price" numeric(10, 2) DEFAULT 0,
+    "updated_at" timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY ("id")
 );
 
