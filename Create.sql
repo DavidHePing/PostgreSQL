6bc4de33562a11fabeaf9113a80ffb2038ecb9e4
Dav He <s991179@gmail.com>
2024-02-22 06:37:57 +0800

    feat: sync data

--- Create.sql
+++ Create.sql
@@ -3,14 +3,20 @@ CREATE TABLE IF NOT EXISTS public."cars"
     "id" serial NOT NULL,
     "name" text NOT NULL,
     "price" numeric(10, 2) DEFAULT 0,
+    "type" text NOT NULL,
     "updated_at" timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    "deleted_at" timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY ("id")
 );
 
 INSERT INTO public."cars"(
-	"name", "price")
-	VALUES ('Tesla', 1000);
+	"name", "price", "type")
+	VALUES ('Tesla', 1000, 'US');
 
 INSERT INTO public."cars"(
-	"name", "price")
-	VALUES ('Toyota', 200);
+	"name", "price", "type")
+	VALUES ('Toyota', 200), 'Japan';
+
+INSERT INTO public."cars"(
+	"name", "price", "type")
+	VALUES ('"Honda"', 150), 'Japan';
